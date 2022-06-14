bbd13eedbd29533f68eb5015db548b52e708af6b
Tanusha-Budi <tanushabudi19@gmail.com>
2022-06-14 18:58:37 +0530

    transactions file

---
+++ transactions.dart
@@ -0,0 +1,12 @@
+class Transactions {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transactions(
+      {required this.id,
+      required this.title,
+      required this.amount,
+      required this.date});
+}
