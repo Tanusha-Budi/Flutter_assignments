d7a53bae805569b9d5953a52906567dfeb6a45e1
Tanusha-Budi <106596688+Tanusha-Budi@users.noreply.github.com>
2022-06-28 19:21:37 +0530

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,100 +1,56 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
-
-import './result.dart';
-import './quiz.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  final _Questions = const [
-    {
-      'Questions': 'What is Flutter?',
-      'Options': [
-        {'text': 'Flutter is an open-source DBMS', 'score': 0},
-        {'text': 'Flutter is an open-source UI toolkit', 'score': 10},
-        {'text': 'Flutter is an open-source backend toolkit', 'score': 0},
-        {'text': 'All the above', 'score': 0}
-      ]
-    },
-    {
-      'Questions': 'What is Dart?',
-      'Options': [
-        {'text': 'Dart is a object-oriented programming language ', 'score': 0},
-        {
-          'text': 'Dart is used to create a frontend user interfaces',
-          'score': 0
-        },
-        {'text': 'Both A and B', 'score': 10},
-        {'text': 'None of the above', 'score': 0}
-      ]
-    },
-    {
-      'Questions':
-          'Which of the following takes more time to compile and update the app?',
-      'Options': [
-        {'text': 'Hot Reload', 'score': 0},
-        {'text': 'Hot Restart', 'score': 10},
-        {'text': 'Cold Reload', 'score': 0},
-        {'text': 'Depends on Computer'}
-      ]
-    },
-    {
-      'Questions':
-          'Without the main() function, we cannot write any program on Flutter',
-      'Options': [
-        {'text': 'True', 'score': 10},
-        {'text': 'False', 'score': 0},
-      ]
-    },
-  ];
-
-  var _Count = 0;
-  var _totalscore = 0;
-  
-  void _resetQuiz() {
-    setState(() {
-      _Count = 0;
-      _totalscore = 0;
-    });
-  }
-
-  void answerQuestion(int score) {
-    _totalscore += score;
-
-    setState(() {
-      _Count = _Count + 1;
-    });
-
-    print(_Count);
-    if (_Count < _Questions.length) {
-      print('We have more questions');
-    } else {
-      print('End of the quiz');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Welcome to the Quiz', textAlign: TextAlign.center),
-            ),
-            body: (_Count < _Questions.length)
-                ? Quiz(
-                    Questions: _Questions,
-                    answerQuestion: answerQuestion,
-                    Count: _Count,
-                  )
-                : Result(_totalscore)));
+      title: 'Flutter Practice',
+
+      // to hide debug banner
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Practice'),
+        ),
+        body: Container(
+          color: Colors.red,
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width,
+          alignment: Alignment.center,
+          child: LayoutBuilder(
+            builder: (BuildContext ctx, BoxConstraints constraints) {
+              return Container(
+                color: Colors.green,
+                alignment: Alignment.center,
+                height: constraints.maxHeight * 0.5,
+                width: constraints.maxWidth * 0.5,
+                child: Text(
+                  'LayoutBuilder Widget',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
